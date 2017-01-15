707ea5bd03305860e9a0fed57c4384c86b2d096f
ramanand07 <ramanand07@gmail.com>
2017-01-15 17:37:06 -0600

    Complete Assignment 2
    Completed the second assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function makeCacheMatrix creates a matrix, and defines functions to inverse the matrix
+## The second function inverses the matrix if it is not already cached
 
-## Write a short comment describing this function
+## This function creates a square matrix and caches the function in the parent environment
 
 makeCacheMatrix <- function(x = matrix()) {
+ 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix) 
 
 }
 
 
-## Write a short comment describing this function
+## This function inverses the matrix passed in its argument using the cached function defined in the
+## first function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+        
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  matrix <- x$get()
+  m <- solve(matrix, ...) ## If not cached, return a matrix that is the inverse of 'x'
+  x$setmatrix(m)
+  m
 }
